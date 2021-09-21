0aa116983b49840c143b7e03e3e20f9e8e4c88bd
NerdyCoder-AJ <68893384+NerdyCoder-AJ@users.noreply.github.com>
2021-09-21 10:36:57 +0530

    Update settings.py

--- DjCart/settings.py
+++ DjCart/settings.py
@@ -131,7 +131,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'akashjalkote55@gmail.com'
-EMAIL_HOST_PASSWORD = 'akash754654'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
  
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
